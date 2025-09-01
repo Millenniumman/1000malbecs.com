55a1e74f51d50fa046d295ec1df015cb8f44a7af
Millenniumman <federico.augspach@gmail.com>
2025-09-01 17:51:51 +0200

    Update index.js

--- workers/index.js
+++ workers/index.js
@@ -28,7 +28,7 @@ export default {
           events: "Eventos",
           info: "Info",
           home: "Volver al inicio",
-          blog: "1000 Historias - Blog", // Añadido
+          blog: "1000 Historias - Blog",
           provinces_list: {
             la_rioja: "La Rioja",
             mendoza: "Mendoza",
@@ -63,7 +63,7 @@ export default {
           events: "Events",
           info: "Info",
           home: "Back to home",
-          blog: "1000 Stories - Blog", // Añadido
+          blog: "1000 Stories - Blog",
           provinces_list: {
             la_rioja: "La Rioja",
             mendoza: "Mendoza",
@@ -98,7 +98,7 @@ export default {
           events: "Veranstaltungen",
           info: "Info",
           home: "Zurück zur Startseite",
-          blog: "1000 Geschichten - Blog", // Añadido
+          blog: "1000 Geschichten - Blog",
           provinces_list: {
             la_rioja: "La Rioja",
             mendoza: "Mendoza",
@@ -232,7 +232,6 @@ export default {
             <img src="/images/1000-malbecs-logo.png" alt="1000malbecs Logo" class="logo">
           </a>
         </div>
-        <a href="/${lang}/blog/index.html" class="nav-link"><i class="fas fa-blog"></i> ${translations[lang].navbar.blog}</a>
         <h2>${translations[lang].navbar.categories}</h2>
         <details>
           <summary><i class="fas fa-map-marker-alt"></i> ${translations[lang].navbar.provinces}</summary>
@@ -252,6 +251,7 @@ export default {
             ${eventLinks}
           </ul>
         </details>
+        <a href="/${lang}/blog/index.html" class="nav-link"><i class="fas fa-book"></i> ${translations[lang].navbar.blog}</a>
         <details>
           <summary><i class="fas fa-wine-glass"></i> ${translations[lang].navbar.info}</summary>
           <ul>
