8cb640d16fe5b85e12226439ccedbe49c81977ce
Millenniumman <federico.augspach@gmail.com>
2026-06-30 14:19:52 +0200

    borro function

--- checkout.js
+++
@@ -1,7 +0,0 @@
-export async function onRequestPost({ request }) {
-  return new Response(JSON.stringify({ 
-    id: "cs_test_" + Date.now() 
-  }), {
-    headers: { 'Content-Type': 'application/json' }
-  });
-}
