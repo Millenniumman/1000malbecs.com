9293b50563fe1e031280ab807514c233f0f5d6d3
Millenniumman <federico.augspach@gmail.com>
2026-06-30 19:44:18 +0200

    Update cart.js

--- js/cart.js
+++ js/cart.js
@@ -1,4 +1,6 @@
 // js/cart.js - Versión FINAL Multilingüe + Envío
+// ==================== STRIPE ====================
+const STRIPE_PUBLISHABLE_KEY = "pk_test_51TbKblCWcVRBeV4k9R59XaDL0oltLt812ixqD5gejGX2RfI3zcazk9f44NEC4XARyIwkuqfqDzy2y3nAfPJ1I8oV00GiQ3cgSE";   // ← Cambia por tu clave de test
 let cart = JSON.parse(localStorage.getItem('cart')) || [];
 
 const translations = {
